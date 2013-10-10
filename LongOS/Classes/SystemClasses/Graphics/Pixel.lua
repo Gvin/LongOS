f18029d021860e01fe874becb37f0409783d71a7
Gvin <gvin.sim@gmail.com>
2013-10-10 14:19:46 +0300

    Increased screen update frequency.

--- LongOS/Classes/SystemClasses/Graphics/Pixel.lua
+++ LongOS/Classes/SystemClasses/Graphics/Pixel.lua
@@ -1,80 +1,107 @@
-Classes.System.Graphics.Pixel = Class(Object, function(this, initBackColor, initTextColor, initSymbol)
+Classes.System.Graphics.Pixel = Class(Object, function(this, _initBackColor, _initTextColor, _initSymbol)
 	Object.init(this, 'Pixel');
 
-	local backgroundColor = initBackColor;
-	local textColor = initTextColor;
+	------------------------------------------------------------------------------------------------------------------
+	----- Fields -----------------------------------------------------------------------------------------------------
+	------------------------------------------------------------------------------------------------------------------
 
-	if (backgroundColor == nil) then
-		backgroundColor = colors.black;
-	end
-	if (textColor == nil) then
-		textColor = colors.white;
-	end
+	local backgroundColor;
+	local textColor;
+	local symbol;
 
-	local symbol = '';
+	------------------------------------------------------------------------------------------------------------------
+	----- Properties -------------------------------------------------------------------------------------------------
+	------------------------------------------------------------------------------------------------------------------
 
-	if (initSymbol ~= nil) then
-		symbol = ''..string.sub(initSymbol, 1, 1);
-	end
-
-	this.GetBackgroundColor = function()
+	function this:GetBackgroundColor()
 		return backgroundColor;
 	end
 
-	this.SetBackgroundColor = function(_, value)
-		if (type(value) ~= 'number') then
-			error('Pixel.SetBackgroundColor: number expected, got '..type(value)..'.');
+	function this:SetBackgroundColor(_value)
+		if (type(_value) ~= 'number') then
+			error('Pixel.SetBackgroundColor [value]: Number expected, got '..type(_value)..'.');
 		end
 
-		backgroundColor = value;
+		backgroundColor = _value;
 	end
 
-	this.GetTextColor = function()
+	function this:GetTextColor()
 		return textColor;
 	end
 
-	this.SetTextColor = function(_, value)
-		if (type(value) ~= 'number') then
-			error('Pixel.SetTextColor: number expected, got '..type(value)..'.');
+	function this:SetTextColor(_value)
+		if (type(_value) ~= 'number') then
+			error('Pixel.SetTextColor [value]: number expected, got '..type(_value)..'.');
 		end
 
-		textColor = value;
+		textColor = _value;
 	end
 
-	this.GetSymbol = function()
+	function this:GetSymbol()
 		return symbol;
 	end
 
-	this.SetSymbol = function(_, value)
-		if (type(value) ~= 'string') then
-			error('Pixel.SetSymbol: string expected, got '..type(value)..'.');
+	function this:SetSymbol(_value)
+		if (type(_value) ~= 'string') then
+			error('Pixel.SetSymbol: string expected, got '..type(_value)..'.');
 		end
 
-		symbol = ''..string.sub(value, 1, 1);
+		local newSymbol = ''..string.sub(_value, 1, 1);
+		symbol = newSymbol;
 	end
 
-	this.DrawAt = function(_, x, y)
-		if (type(x) ~= 'number') then
-			error('Pixel.DrawAt [x]: number expected, got '..type(x)..'.');
+	------------------------------------------------------------------------------------------------------------------
+	----- Methods ----------------------------------------------------------------------------------------------------
+	------------------------------------------------------------------------------------------------------------------
+
+	function this:DrawAt(_x, _y)
+		if (type(_x) ~= 'number') then
+			error('Pixel.DrawAt [x]: number expected, got '..type(_x)..'.');
 		end
 
-		if (type(y) ~= 'number') then
-			error('Pixel.DrawAt [y]: number expected, got '..type(y)..'.');
+		if (type(_y) ~= 'number') then
+			error('Pixel.DrawAt [y]: number expected, got '..type(_y)..'.');
 		end
 
 		if (symbol == '') then
-			paintutils.drawPixel(x, y, backgroundColor);
+			paintutils.drawPixel(_x, _y, backgroundColor);
 		else
 			term.setTextColor(textColor);
 			term.setBackgroundColor(backgroundColor);
-			term.setCursorPos(x, y);
+			term.setCursorPos(_x, _y);
 			term.write(symbol);
 		end
 	end
 
-	this.Clear = function()
+	function this:Clear()
 		backgroundColor = colors.black;
 		textColor = colors.white;
 		symbol = '';
 	end
+
+	------------------------------------------------------------------------------------------------------------------
+	----- Constructors -----------------------------------------------------------------------------------------------
+	------------------------------------------------------------------------------------------------------------------
+
+	local function constructor(_initBackColor, _initTextColor, _initSymbol)
+		if (_initBackColor ~= nil and type(_initBackColor) ~= 'number') then
+			error('Pixel.Constructor [initBackColor]: Number or nil expected, got '..type(_initBackColor)..'.');
+		end
+		if (_initTextColor ~= nil and type(_initTextColor) ~= 'number') then
+			error('Pixel.Constructor [initTextColor]: Number or nil expected, got '..type(_initTextColor)..'.');
+		end
+		if (_initSymbol ~= nil and type(_initSymbol) ~= 'string') then
+			error('Pixel.Constructor [initSymbol]: String or nil expected, got '..type(_initSymbol)..'.');
+		end
+
+		backgroundColor = _initBackColor or colors.black;
+		textColor = _initTextColor or colors.white;
+
+		symbol = '';
+		if (initSymbol ~= nil) then
+			symbol = ''..string.sub(initSymbol, 1, 1);
+		end
+	end
+
+	constructor(_initBackColor, _initTextColor, _initSymbol)
 end)
